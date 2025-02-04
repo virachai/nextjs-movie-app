682e496447b7b6cd11fdd3b4656bd94e3d394617
Virachai Wongsena <wvirachai@gmail.com>
2025-02-04 13:41:20 +0700

    edit props searchParams

--- src/app/home/[category]/page.tsx
+++ src/app/home/[category]/page.tsx
@@ -1,18 +1,22 @@
 import { MediaCategory, Movie } from "@/types";
 import MovieCard from "../_components/MovieCard";
 import { fetchMediasByCategory } from "./fetchMediaByCategory.helper";
+// import { PageProps } from "next";
 
+// Update the Props type to match Next.js expectations
 type Props = {
   params: {
-    category: MediaCategory;
+    category: string;
   };
+  searchParams?: { [key: string]: string | string[] | undefined };
 };
 
 export default async function CategoryPage({ params: { category } }: Props) {
   let mediaData: Movie[] = [];
-
   try {
-    mediaData = await fetchMediasByCategory(category);
+    // You might need to validate/cast the category parameter
+    const validCategory = category as MediaCategory;
+    mediaData = await fetchMediasByCategory(validCategory);
   } catch (error) {
     console.error("Failed to fetch media data:", error);
   }
@@ -24,8 +28,6 @@ export default async function CategoryPage({ params: { category } }: Props) {
         <div className="flex justify-center items-center px-[6%] h-[10rem] font-[900] text-4xl text-center text-white capitalize leading-[1.2] cursor-default select-none Helvetica Neue, Roboto, sans-serif">
           {category}
         </div>
-
-        {/* Render MovieCards */}
         <div className="gap-4 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
           {mediaData.length > 0 ? (
             mediaData.map((movie) => <MovieCard key={movie.id} movie={movie} />)
