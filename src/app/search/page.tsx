d670cc14f0fab47f9818cb7c6151f90ce2921466
Virachai Wongsena <wvirachai@gmail.com>
2025-02-04 14:15:14 +0700

    Suspense for SearchBar

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -7,7 +7,9 @@ export default function HomePage() {
     <>
       <h1 className="hidden">Netflix Search</h1>
       <div className="flex flex-col mt-20 min-h-[100vh]">
-        <SearchBar />
+        <Suspense fallback={<div>Loading...</div>}>
+          <SearchBar />
+        </Suspense>
         <Suspense fallback={<div>Loading...</div>}>
           <SearchContainer />
         </Suspense>
