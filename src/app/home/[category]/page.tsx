12049729326f982f701cd0309d700ca7e9a3a479
Virachai Wongsena <wvirachai@gmail.com>
2025-02-04 14:23:42 +0700

    update category

--- src/app/home/[category]/page.tsx
+++ src/app/home/[category]/page.tsx
@@ -1,11 +1,8 @@
-// import { MediaCategory, Movie } from "@/types";
-// import MovieCard from "../_components/MovieCard";
-// import { fetchMediasByCategory } from "./fetchMediaByCategory.helper";
-// import { categoryMap } from "@/lib/categoryMap"; // import the category mapping
-// src/app/home/[category]/page.tsx
-import { MediaCategory } from "@/types"; // Adjust import based on your project
-
-import * as React from "react";
+import { MediaCategory, Movie } from "@/types";
+import MovieCard from "../_components/MovieCard";
+import { fetchMediasByCategory } from "./fetchMediaByCategory.helper";
+import { categoryMap } from "@/lib/categoryMap"; // import the category mapping
+// import * as React from "react";
 // Correct type definition for PageProps
 type PageProps = {
   params: Promise<{ category: MediaCategory }>;
@@ -13,13 +10,38 @@ type PageProps = {
 
 // The Page component receives props with the correct type
 const Page = async ({ params }: PageProps) => {
-  // const { category } = params;
-  const { category } = await params;
+  const { category } = await params; // Assuming `slug` is part of the params
+  let mediaData: Movie[] = [];
 
+  try {
+    // Map the slug to the actual category
+    const validCategory = category as MediaCategory;
+    mediaData = await fetchMediasByCategory(validCategory);
+  } catch (error) {
+    console.error("Failed to fetch media data:", error);
+  }
+
+  // return (
+  //   <div>
+  //     <h1>Category: {category}</h1>
+  //     {/* Additional content based on category, e.g., render mediaData */}
+  //   </div>
+  // );
   return (
-    <div>
-      <h1>Category: {category}</h1>
-      {/* Additional content based on category */}
+    <div className="px-[1.5rem] sm:px-8 lg:px-16">
+      <h1 className="hidden">Netflix {category}</h1>
+      <div className="flex flex-col mt-20 min-h-[100vh]">
+        <div className="flex justify-center items-center px-[6%] h-[10rem] font-[900] text-4xl text-center text-white capitalize leading-[1.2] cursor-default select-none Helvetica Neue, Roboto, sans-serif">
+          {category}
+        </div>
+        <div className="gap-4 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+          {mediaData.length > 0 ? (
+            mediaData.map((movie) => <MovieCard key={movie.id} movie={movie} />)
+          ) : (
+            <p className="text-center text-white">No {category} found.</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
