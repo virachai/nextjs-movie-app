9e4bdb4187b3b6e9bb641abe0eb837570ee90a87
Virachai Wongsena <wvirachai@gmail.com>
2025-02-05 10:33:01 +0700

    use aspect 10/15 on movie card

--- src/app/home/_components/MovieCard.tsx
+++ src/app/home/_components/MovieCard.tsx
@@ -14,8 +14,11 @@ interface MovieCardProps {
 function MovieCard({ movie, large }: MovieCardProps) {
   const baseClasses =
     "relative cursor-pointer max-w-[280px] min-w-[100px] w-full";
-  const largeClasses = large ? "h-52 sm:h-80" : "h-[150px] sm:h-[130px]";
-  const largeClassesOverlay = large ? "h-80" : "h-80";
+
+  // Use aspect-ratio for large view
+  const largeClasses = large ? "aspect-[10/15]" : "h-[150px] sm:h-[130px]"; // For non-large view, keep default height
+
+  // const largeClassesOverlay = large ? "aspect-[10/15]" : "aspect-[10/15]";
   const imageSrc = large ? movie.poster_path : movie.backdrop_path;
 
   return (
@@ -43,7 +46,7 @@ function MovieCard({ movie, large }: MovieCardProps) {
       </Link>
 
       <div
-        className={`hidden sm:flex relative opacity-0 hover:opacity-100 ${largeClassesOverlay} transition duration-300 sm:hover:scale-125 hover:z-30`}
+        className={`hidden sm:flex relative opacity-0 hover:opacity-100 aspect-[10/15] transition duration-300 sm:hover:scale-125 hover:z-30`}
       >
         <div className="flex justify-center items-center bg-gradient-to-b from-transparent via-black/50 to-black size-full">
           <Image
