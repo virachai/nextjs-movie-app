92bdb268bf4297d5f5659e9312f7930c53eb1e11
Virachai Wongsena <wvirachai@gmail.com>
2025-02-06 08:54:05 +0700

    check session in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 // src/app/page.tsx
 import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/authOptions";
 
 export default async function RedirectPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return redirect("/login");
+  }
   return redirect("/home");
 }
